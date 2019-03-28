9d115af050ca0b96a97189ea9dcc4c5836396ec6
Trajkov Dimitar <45421293+trajkovdimitar@users.noreply.github.com>
2019-03-28 17:45:35 +0100

    Add files via upload

---
+++ Lecture 01/Lecture 01/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lecture_01
+{
+    class SparseArray
+    {
+        public SparseArray()
+        {
+
+        }
+        static SparseArray()
+        {
+
+        }
+    }
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region MyRegion
+            //    Dictionary<string, string> states = new Dictionary<string, string>
+            //    {
+            //        { "skopje", "macedonia" },
+            //        { "berlin", "germany" },
+            //        { "tirana", "albania" },
+            //        { "belgrade", "serbia" },
+            //        { "sofia", "bulgaria" }
+
+            //    };
+            #endregion
+            #region excercise 1
+            //string[] Inputs = new string[2];
+            //Console.WriteLine("Please enter city or a country:");
+            //string input = Console.ReadLine().ToLower();
+            //foreach (var item in states)
+            //{
+            //    if (item.Key == input || item.Value == input)
+            //    {
+            //        Inputs[0] = item.Key;
+            //        Inputs[1] = item.Value;
+            //    }
+            //}
+
+            //if (input == Inputs[0])
+            //{
+            //    Console.WriteLine(input + " is the capital of: " + Inputs[1]);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("The major city of " + input + " is: " + Inputs[0]);
+            //}
+            Console.ReadLine();
+        }
+            #endregion
+        class PrivateConstructor
+        {
+
+
+        }
+    }
+    
+}
+
