591d02e63386102d41a19cb76baa23f0eca2a75b
Trajkov Dimitar <45421293+trajkovdimitar@users.noreply.github.com>
2019-04-02 16:33:18 +0200

    Add files via upload

---
+++ ExtensionMethods/ExtensionMethods/Extensions.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ExtensionMethods
+{
+    public static class Extensions
+    {
+        public static bool IsPalindrom(this string value)
+        {
+            int min = 0;
+            int max = value.Length - 1;
+            while (true)
+            {
+                if (min > max)
+                {
+                    return true;
+                }
+                char a = value[min];
+                char b = value[max];
+                if (char.ToLower(a) != char.ToLower(b))
+                {
+                    return false;
+                }
+                min++;
+                max--;
+            }
+        }
+        public static bool IsPalindrome(this double num)
+        {
+            double r, sum = 0, t;
+            for (t = num; num != 0; num = num / 10)
+            {
+                r = num % 10;
+                sum = sum * 10 + r;
+            }
+            if (t == sum)
+                return true;
+            else
+                return false;
+        }
+    }
+}
---
+++ ExtensionMethods/ExtensionMethods/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExtensionMethods;
+using System.Collections;
+using System;
+
+namespace ExtensionMethods
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string phrase;
+            phrase = "Madam, I`m Adam";
+            Console.WriteLine(phrase.IsPalindrom());
+            phrase = "Madam, I am Adam";
+            Console.WriteLine(phrase.IsPalindrom());
+            phrase = "Refer , refer";
+            Console.WriteLine(phrase.IsPalindrom());
+            Console.WriteLine("-----------------");
+            double numeric;
+            numeric = 123.321;
+            Console.WriteLine(numeric.IsPalindrome());
+            Console.ReadLine();            
+        }
+    }
+}
