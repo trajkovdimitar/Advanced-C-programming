2f2a6d3ca31f8ddd789012a243c2bf347d6badf5
Trajkov Dimitar <45421293+trajkovdimitar@users.noreply.github.com>
2019-04-04 17:58:37 +0200

    Add files via upload

---
+++ Palindrom/Palindrom/Extensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrom
+{
+   public static class Extensions
+    {
+        public static bool IsPalindrom<T>(this T text)
+        {
+            string forwards = text.ToString().ToLower();
+            StringBuilder sb = new StringBuilder();
+            foreach (var item in forwards)
+            {
+                if (Char.IsLetterOrDigit(item))
+                {
+                    sb.Append(item);
+                }                
+            }
+            forwards = sb.ToString();
+            char[] chars = forwards.ToCharArray();
+            Array.Reverse(chars);
+            string backwards = new string(chars);
+            return backwards == forwards;
+        }
+        
+    }
+}
---
+++ Palindrom/Palindrom/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Palindrom
+{
+    // Palindrom checker for all data-types!
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string phrase = "Madam, I`m Adam!";
+            Console.WriteLine(phrase.IsPalindrom());
+            Console.ReadLine();
+        }
+    }
+}
